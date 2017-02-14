1eef47a9408a39fb0a8da2050c7731ff280a6fd4
Harrison McKenzie <hjmck123@gmail.com>
2017-02-14 15:22:44 +1100

    eslint and bulma css framework.

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "camelcase": 1,
+        "eol-last": 1,
+        "no-lonely-if": 1,
+        
+    }
+};
